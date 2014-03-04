dbe75f1df1744514d80fafa234e6ab46b5e42afa
Thomas Levine <_@thomaslevine.com>
2014-03-04 13:13:53 +0000

    fancy

--- nyc.sh
+++ nyc.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
-echo First NYC Shell meeting! March 20-something at some place in New York
+echo '
+First NYC Shell meeting! March 20-something at some place in New York
 
-echo Something cool, by Jeroen Janssens
-
-
+* Something cool, by Jeroen Janssens
 * Tests for the shell, by Thomas Levine
 * Data pipelines, by someone else
+
+Respond if you please by running this file.
+
+    if which wget > /dev/null; then
+      alias get='wget -O -'
+    elif which curl > /dev/null; then
+      aiias get=curl
+    else
+      get() {
+        printf "Download this file and run it.\n$1" > /dev/stderr
+      }
+    fi
+    get http://nyc.sh/nyc.sh | sh
+
+'
