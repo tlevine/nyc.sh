9d5f6eb31603be24e5752f956d4de8e1a2afee82
Thomas Levine <_@thomaslevine.com>
2014-03-04 13:40:21 +0000

    allow for tests of functions

--- nyc.sh
+++ nyc.sh
@@ -48,29 +48,33 @@ validate_number() {
   return test -z $(echo "$x" | tr -d '[0-9]')
 }
 
-# HTTP GET requests
-if which wget > /dev/null; then
-  alias get='wget -O -'
-elif which curl > /dev/null; then
-  aiias get=curl
-else
-  get() {
-    echo 'Copy this to an email, and send it to _@thomaslevine.com.'
-    echo ""
-  }
+main() {
+  # HTTP GET requests
+  if which wget > /dev/null; then
+    alias get='wget -O -'
+  elif which curl > /dev/null; then
+    aiias get=curl
+  else
+    get() {
+      echo 'Copy this to an email, and send it to _@thomaslevine.com.'
+      echo ""
+    }
+  fi
+
+  # Respond if you please.
+  echo Respond if you please.
+  echo
+  name=$(ask 'What is your name?' 'Please type your name and hit enter' 'test -n')
+  email_address=$(ask 'What is your email address?' 'Please type your email address and hit enter.' 'test -n')
+  raw_guests=$(ask 'Is anyone else coming with you? (y/N)' '"y" or "Y" for yes, "n" or "N" for no' validate_yes_no)
+  
+  if test "$raw_guests" = y; then
+    guests=$(ask 'How many people other than you?' 'Please enter a number, like "1" or "2".' )
+  else
+    guests=0
+  fi
+}
+
+if test -z $TESTING; then
+  main
 fi
-
-# Respond if you please.
-echo Respond if you please.
-echo
-name=$(ask 'What is your name?' 'Please type your name and hit enter' 'test -n')
-email_address=$(ask 'What is your email address?' 'Please type your email address and hit enter.' 'test -n')
-raw_guests=$(ask 'Is anyone else coming with you? (y/N)' '"y" or "Y" for yes, "n" or "N" for no' validate_yes_no)
-
-if test "$raw_guests" = y; then
-  guests=$(ask 'How many people other than you?' 'Please enter a number, like "1" or "2".' )
-else
-  guests=0
-fi
-
-# get 
