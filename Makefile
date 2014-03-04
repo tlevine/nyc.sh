7b9e1cdcf2633dc582575f2bbb65757bb9017672
Thomas Levine <_@thomaslevine.com>
2014-03-04 13:00:39 +0000

    make

---
+++ Makefile
@@ -0,0 +1,4 @@
+index.html:
+	printf partials/head > index.html
+	printf nyc.sh > index.html
+	printf partials/foot > index.html
---
+++ nyc.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo First NYC Shell meeting! March 20-something at some place in New York
+
+echo Something cool, by Jeroen Janssens
+
+
+* Tests for the shell, by Thomas Levine
+* Data pipelines, by someone else
