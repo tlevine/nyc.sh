cf17a33ed908f43524bdd0f89d9ea2f946b4a0ce
Thomas Levine <_@thomaslevine.com>
2014-04-10 13:03:03 +0000

    schedule

--- nyc.sh
+++ nyc.sh
@@ -1,84 +1,25 @@
-#!/bin/sh
-set -e
-echo '
-First NYC Shell meeting! April 16 at some place in New York
-
-* Something cool, by Jeroen Janssens
-* Tests for the shell, by Thomas Levine
-* Data pipelines, by someone else
-
-Respond if you please by running this file. Either of these should work.
-
-    wget -O - http://nyc.sh/nyc.sh | sh
-    curl http://nyc.sh/nyc.sh | sh
-
-' > /dev/null
-
-# Ask a question
-ask() {
-  question="$1"
-  error_message="$2"
-  validation_function="$3"
-  printf "$question " > /dev/stderr
-  read x
-  while ! $validation_function $x; do
-    printf "$error_message " > /dev/stderr
-    read x
-  done
-  echo "$x"
-}
-
-# Validations
-validate_yes_no() {
-  anysize="$1"
-  miniscule=$(echo "$anysize" | tr '[YN]' '[yn]')
-  test "$miniscule" = y || test "$miniscule" = n || test -z "$miniscule"
-  return "$?"
-}
-validate_number() {
-  x="$1"
-  echo "$x" | grep  '[0-9]' > /dev/null && test -z $(echo "$x" | tr -d '[0-9]')
-  return "$?"
-}
-
-url=http://nyc.sh/rsvp # Switch this for another form processer so we don't have to run it.
-get_wget() {
-  wget --post-data "name=$1&email.address=$2&how.many=$3" "$url"
-}
-get_curl() {
-  curl -d "name=$1" -d "email.address=$2" -d "how.many=$3" "$url"
-}
-get_csv() {
-  echo 'Copy this to an email, and send it to _@thomaslevine.com.'
-  echo
-  echo "name,email.address,how.many"
-  echo "$1,$2,$3"
-}
-
-main() {
-  # HTTP GET requests
-  if which wget > /dev/null; then
-    alias get=get_wget
-  elif which curl > /dev/null; then
-    alias get=get_curl
-  else
-    alias get=get_csv
-  fi
-
-  echo Respond if you please.
-  echo
-  name=$(ask 'What is your name?' 'Please type your name and hit enter' 'test -n')
-  email_address=$(ask 'What is your email address?' 'Please type your email address and hit enter.' 'test -n')
-  raw_guests=$(ask 'Is anyone else coming with you? (y/N)' '"y" or "Y" for yes, "n" or "N" for no' validate_yes_no)
-  
-  if test "$raw_guests" = y; then
-    guests=$(ask 'How many people other than you?' 'Please enter a number, like "1" or "2".' )
-    how_many=$(($guests + 1))
-  else
-    how_many=1
-  fi
-}
-
-if test -z $TESTING; then
-  main
-fi
+# New York City Shell User Group
+# 
+# First meeting
+# April 16, 7 pm
+# Enigma office, 520 Broadway, SoHo
+#
+# Schedule
+# --------
+# First, we'll have a talk on testing shell programs by
+# Thomas Levine (http://thomaslevine.com). Tom will tell
+# you about the various strategies for testing shell
+# programs and explain why he wrote his own test framework,
+# Urchin (https://github.com/scraperwiki/urchin).
+#
+# Next, we'll have five-minute lightning talks by anyone
+# who is interested. If you're coming, you definitely have
+# something to talk about; here are some things you could
+# talk about.
+#
+# * Confusing or neat shell features
+# * Your shell profile
+# * Why you prefer one shell over another
+# * A shell program you've written
+#
+# Respond if you please by emailing 
